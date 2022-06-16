00edc6b0bd4634cfa43dc974822b0a6e4966cccc
Arthur Imamutdinov <94892392+ivs-code@users.noreply.github.com>
2022-06-16 09:44:29 +0500

    Небольшая шпаргалка с типами данных
    Добавил типы данных

---
+++ data-types.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+  /*
+    9 - типов данных
+
+    1. Integer - 123(числа)
+    2. String - "fjfjjf"(строки)
+    3. Float - 3.5(число с плавающей точкой)
+    4. Boolean - true, false
+    5. Null
+    6. Array [массив]
+    7. Object
+    8. Callable
+    9. Resourse
+
+  */?>
+</body>
+</html>
