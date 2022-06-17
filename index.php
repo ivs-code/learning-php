d60bb7add01c1d8da7777f46f330d71b4802539d
Arthur Imamutdinov <94892392+ivs-code@users.noreply.github.com>
2022-06-18 03:06:32 +0500

    Главная страница

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Примеры кода</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <style>
+        * {
+            box-sizing: border-box;
+            font-family: Montserrat;
+        }
+        body {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        li {
+            list-style: none;
+            padding: 10px 0;
+        }
+        li a {
+            color: #524e4e;
+            text-decoration: none;
+        }
+        li a:hover {
+            color: #000000;
+            font-weight: 600;
+        }
+    </style>
+</head>
+<body>
+    <div class="content">
+        <h1>Навигация по моему обучению php</h1>
+        <li><a href="variables.php">Сложение двух чисел с помощью переменных</a></li>
+        <li><a href="loops.php">Примеры с циклами</a></li>
+        <li><a href="if.php">Пример условий и цикла</a></li>
+        <li><a href="get-test1.php">Применение массива $_GET(отправка)</a></li>
+        <li><a href="get-test2.php">Применение массива $_GET(получение)</a></li>
+        <li><a href="form.php">Пример c формой POST(отправка)</a></li>
+        <li><a href="form1.php">Пример с формой POST(получение)</a></li>
+        <li><a href="functions.php">Функции</a></li>
+        <li><a href="data-types.php">Небольшая шпаргалка с типами данных</a></li>
+        <li><a href="cookies.php">Работа с сессией и куками</a></li>
+        <li><a href="arrays.php">Пример работы с массивами</a></li>
+
+        <p>Весь код доступен на <a href="https://github.com/ivs-code/learning-php">Github</a></p>
+    </div>
+</body>
+</html>
